7f03ab7d46377fb6f894882bd26effa6b6520a3e
Dan <76439485+dann-5m@users.noreply.github.com>
2022-09-13 16:07:59 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,5 @@
 fx_version "cerulean"
 
-description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
-version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
-
 lua54 'yes'
 
 games {
